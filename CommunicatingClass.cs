9bafdae4f853e3ad194045199ed3053d7ce47a65
CasianLW <ciorbacasian@gmail.com>
2024-01-22 16:14:38 +0100

    Ajoutez des fichiers projet.

---
+++ CommunicatingClass.cs
@@ -0,0 +1,19 @@
+﻿public delegate void GetPlayerAction(string action);
+public delegate void InformPlayer(string message);
+
+public class CommunicatingClass
+{
+    public GetPlayerAction OnGetPlayerAction;
+    public InformPlayer OnInformPlayer;
+
+    // Changez 'protected' en 'public' pour permettre l'accès depuis l'extérieur et vice versa
+    public void RequestPlayerAction()
+    {
+        OnGetPlayerAction?.Invoke("ActionRequested");
+    }
+
+    public void SendInfoToPlayer(string message)
+    {
+        OnInformPlayer?.Invoke(message);
+    }
+}
