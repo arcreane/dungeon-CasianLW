69c4765a38ecb190878ba016f84f3a7e35acc545
CasianLW <ciorbcasian@gmail.com>
2024-01-22 16:14:38 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleUserInterface.cs
@@ -0,0 +1,14 @@
+﻿
+
+public interface IUserInterface
+{
+    void Display(string message);
+}
+
+public class ConsoleUserInterface : IUserInterface
+{
+    public void Display(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Create an instance of the user interface
+        IUserInterface userInterface = new ConsoleUserInterface();
+
+        // Create an instance of CommunicatingClass
+        CommunicatingClass gameCommunication = new CommunicatingClass();
+
+        // Associate the delegates with the corresponding methods
+        gameCommunication.OnGetPlayerAction = (action) =>
+        {
+            // Simulate receiving an action from the player
+            userInterface.Display($"Player action received: {action}");
+        };
+        gameCommunication.OnInformPlayer = (message) =>
+        {
+            // Use the user interface to display the message
+            userInterface.Display(message);
+        };
+
+        // Test the delegates
+        gameCommunication.RequestPlayerAction();
+        gameCommunication.SendInfoToPlayer("Welcome to the game!");
+
+    }
+}
